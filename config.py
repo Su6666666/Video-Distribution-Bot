5b838ae0aace2bfa75bd4b36b03658cb2ffe425d
𝐒𝐮𝐛𝐡𝐚𝐣𝐢𝐭 𝐆𝐡𝐨𝐬𝐡 <86417238+Su6666666@users.noreply.github.com>
2026-02-07 00:35:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,20 @@
 import os
 
 class Config:
+    # Telegram API Details
     API_ID = int(os.environ.get("API_ID", "0"))
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    
+    # Database Details
     MONGO_URI = os.environ.get("MONGO_URI", "")
     DB_NAME = os.environ.get("DB_NAME", "VideoBot")
+    
+    # Force Subscribe Details
     FSUB_CHANNEL = int(os.environ.get("FSUB_CHANNEL", "-100")) 
     FSUB_LINK = os.environ.get("FSUB_LINK", "https://t.me/StreamAlertsIndia")
     
+    # Video Channels
     CHANNELS = {
         "indian": int(os.environ.get("INDIAN_CH", "-100")),
         "english": int(os.environ.get("ENGLISH_CH", "-100")),
@@ -17,8 +23,8 @@ class Config:
         "viral": int(os.environ.get("VIRAL_CH", "-100"))
     }
     
-    # শর্টনার ও ভেরিফাই সেটিংস
+    # Link Shortener Settings
     SHORTENER_URL = os.environ.get("SHORTENER_URL", "")
     SHORTENER_API = os.environ.get("SHORTENER_API", "")
     SHORTENER_ON = os.environ.get("SHORTENER_ON", "False").lower() == "true"
-    [span_4](start_span)VERIFY_EXPIRE = 86400 # ২৪ ঘণ্টা (সেকেন্ডে)[span_4](end_span)
+    VERIFY_EXPIRE = 86400 # 24 Hours
